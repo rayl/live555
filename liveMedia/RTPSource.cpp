da6ebb106ac4d0906cc5ece53d4d5b88a3d6fa3a
Ray Lehtiniemi <rayl@mail.com>
2009-05-05 20:24:37 -0600

    Import live.2006.09.18.tar.gz
    - Fixed a bug in the way that "RTPSource" records the maximum sequence number seen so far.
      (The bug could be triggered by packets arriving out of order.)  Thanks to Mallikharjuna Reddy
      for pointing this out.
    - When creating new datagram sockets, we now attempt to setsockopt() SO_REUSEPORT and IP_MULTICAST_LOOP,
      even on Windows.  (A version of Windows prior to XP had problems with this, but it's apparently no longer
      a problem.)
    
    Signed-off-by: Ray Lehtiniemi <rayl@mail.com>

--- liveMedia/RTPSource.cpp
+++ liveMedia/RTPSource.cpp
@@ -247,19 +247,21 @@ void RTPReceptionStats
     ++fTotBytesReceived_hi;
   }
 
-  // Check whether the sequence number has wrapped around:
-  unsigned seqNumCycle = (fHighestExtSeqNumReceived&0xFFFF0000);
+  // Check whether the new sequence number is the highest yet seen:
   unsigned oldSeqNum = (fHighestExtSeqNumReceived&0xFFFF);
-  unsigned seqNumDifference = (unsigned)((int)seqNum-(int)oldSeqNum);
-  if (seqNumDifference >= 0x8000
-      && seqNumLT((u_int16_t)oldSeqNum, seqNum)) {
-    // sequence number wrapped around => start a new cycle:
-    seqNumCycle += 0x10000;
-  }
+  if (seqNumLT((u_int16_t)oldSeqNum, seqNum)) {
+    // This packet was not an old packet received out of order, so check it:
+    unsigned seqNumCycle = (fHighestExtSeqNumReceived&0xFFFF0000);
+    unsigned seqNumDifference = (unsigned)((int)seqNum-(int)oldSeqNum);
+    if (seqNumDifference >= 0x8000) {
+      // The sequence number wrapped around, so start a new cycle:
+      seqNumCycle += 0x10000;
+    }
 
-  unsigned newSeqNum = seqNumCycle|seqNum;
-  if (newSeqNum > fHighestExtSeqNumReceived) {
-    fHighestExtSeqNumReceived = newSeqNum;
+    unsigned newSeqNum = seqNumCycle|seqNum;
+    if (newSeqNum > fHighestExtSeqNumReceived) {
+      fHighestExtSeqNumReceived = newSeqNum;
+    }
   }
 
   // Record the inter-packet delay
